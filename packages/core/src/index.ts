988c6ecac1d5e1f1ab12d85e9df09f3d72b74382
Aleksandr Neyasov <a.neyasov92@gmail.com>
2022-05-01 12:56:56 +0400

    DelegatedFlatStateVariant's implementation

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./taggedState";
+export * from "./delegatedTaggedState";
 export { default as mkts } from "./mkts";
 export { default as UnexpectedStateError } from "./UnexpectedStateError";
