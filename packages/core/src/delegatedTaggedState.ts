d8154e1df95ee0cee6d3be9dc4d32727a4c870e5
Aleksandr Neyasov <a.neyasov92@gmail.com>
2022-05-02 10:38:00 +0400

    move DelegatedFlatStateVariant into taggedState.ts and add a test to ensure type equality between the flat state and the corresponding tagged state.

--- packages/core/src/delegatedTaggedState.ts
+++
@@ -1,6 +0,0 @@
-import { StateVariant } from "./taggedState";
-
-export type DelegatedFlatStateVariant<
-  Tag extends string,
-  To extends { [K in "tag" | string]: K extends "tag" ? Tag : To[K] }
-> = StateVariant<Tag, Omit<Extract<To, { tag: Tag }>, "tag">>;
